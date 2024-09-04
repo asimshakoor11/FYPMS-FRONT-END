abbfe053e25f009e191749c31413dbe39d4006c7
Asim Shakoor <asimshakoor920830@gmail.com>
2024-09-04 14:35:32 +0500

    room code

--- src/Components/Room.jsx
+++ src/Components/Room.jsx
@@ -1,11 +1,9 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import { useParams, useLocation } from 'react-router-dom';
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
-import axios from 'axios';
 import AudioRecorder from "./AudioRecorder";
 
 const Room = () => {
-
   const { roomID } = useParams(); // Get RoomCode from URL path
   const query = new URLSearchParams(useLocation().search);
   const meetingAgenda = query.get('agenda');
@@ -14,44 +12,50 @@ const Room = () => {
   const role = localStorage.getItem("userRole");
   const name = localStorage.getItem("loggedInUserName");
 
+  const meetingContainerRef = useRef(null); // Ref for the meeting container
 
-  // const roomID  = RoomCode
-  const meeting = async (element) => {
-    const appID = 946219318;
-    const serverSecret = "8e0b853d79deae0bcbfe949b73ca46a4";
-    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-      appID,
-      serverSecret,
-      roomID,
-      Date.now().toString(),
-      name
-    );
-    const zp = ZegoUIKitPrebuilt.create(kitToken);
+  useEffect(() => {
+    const meeting = async () => {
+      const appID = 946219318;
+      const serverSecret = "8e0b853d79deae0bcbfe949b73ca46a4";
+      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+        appID,
+        serverSecret,
+        roomID,
+        Date.now().toString(),
+        name
+      );
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
 
-    zp.joinRoom({
-      container: element,
-      scenario: {
-        mode: ZegoUIKitPrebuilt.GroupCall,
-      },
-    });
-  };
+      zp.joinRoom({
+        container: meetingContainerRef.current,
+        scenario: {
+          mode: ZegoUIKitPrebuilt.GroupCall,
+        },
+      });
+    };
 
+    if (meetingContainerRef.current) {
+      meeting();
+    }
+  }, [roomID, name]);
 
   return (
-    <div className="flex bg-white">
-      <div ref={meeting} className={` ${role === 'Student' ? 'w-[100%]' : 'w-[80%]'} h-full`} ></div>
+    <div className="flex bg-white h-screen">
+      <div
+        ref={meetingContainerRef}
+        className={`${role === 'Student' ? 'w-full' : 'w-4/5'} h-full`}
+      ></div>
       {
-        role === 'Student' ? (
-          <>
-          </>
-        ) : (
+        role !== 'Student' && (
           <div style={{ width: "20%", height: "100%" }}>
             <AudioRecorder meetingAgenda={meetingAgenda} number={number} />
           </div>
         )
       }
-    </div >
+    </div>
   );
 };
 
 export default Room;
+ 
